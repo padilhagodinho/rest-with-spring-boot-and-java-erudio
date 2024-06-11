0861bd08cd0e7c311ac78b195d9cb39f6e7226f3
padgodin <luizhenriquepadilha@gmail.com>
2024-06-11 11:13:57 -0400

    Implementando Evolutionary Database Design com Flyway

---
+++ 11_Migration_with_Flyway/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,5 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Berlandia - Minas - Brasel', 'Leandro 1', 'Male', 'Costa 1'),
+	(4, 'Berlandia - Minas - Brasel', 'Leandro 2', 'Male', 'Costa 2'),
+	(9, 'Berlandia - Minas - Brasel', 'Leandro 3', 'Male', 'Costa 3'),
+	(10, 'Berlandia - Minas - Brasel', 'Leandro 4', 'Male', 'Costa 4');
